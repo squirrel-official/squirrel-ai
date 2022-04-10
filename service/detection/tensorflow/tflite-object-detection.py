15cb9ad5e67de49948fcdbcbeb19839e3635120d
anil <anil.kumar.ait09@gmail.com>
2022-04-11 00:54:44 +1000

    Squirrel-AI - Added support for camera

--- service/detection/tensorflow/tflite-object-detection.py
+++ service/detection/tensorflow/tflite-object-detection.py
@@ -128,58 +128,67 @@ if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
 else:  # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-# Loop over every image and perform detection
-image_path = "/Users/anil/Desktop/1.png"
-print("starting")
-# Load image and resize to expected shape [1xHxWx3]
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-imH, imW, _ = image.shape
-image_resized = cv2.resize(image_rgb, (width, height))
-input_data = np.expand_dims(image_resized, axis=0)
-print("starting1")
-# Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-if floating_model:
-    input_data = (np.float32(input_data) - input_mean) / input_std
+capture = cv2.VideoCapture(0)
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+# capture.set(cv2.CAP_PROP_FPS, 5)
 
-# Perform the actual detection by running the model with the image as input
-interpreter.set_tensor(input_details[0]['index'], input_data)
-interpreter.invoke()
-print("starting3")
-# Retrieve detection results
-boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
-    0]  # Bounding box coordinates of detected objects
-classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
-scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
-print("starting4")
-print(len(scores))
-# Loop over all detections and draw detection box if confidence is above minimum threshold
-for i in range(len(scores)):
-    if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
-        # Get bounding box coordinates and draw box
-        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-        ymin = int(max(1, (boxes[i][0] * imH)))
-        xmin = int(max(1, (boxes[i][1] * imW)))
-        ymax = int(min(imH, (boxes[i][2] * imH)))
-        xmax = int(min(imW, (boxes[i][3] * imW)))
+if not capture.isOpened():
+    print("Error opening video stream or file")
+count = 0;
+while capture.isOpened():
+    _, image = capture.read()
+    # Loop over every image and perform detection
+    print("starting")
+    # Load image and resize to expected shape [1xHxWx3]
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    print("starting 0.5")
+    imH, imW, _ = image.shape
+    image_resized = cv2.resize(image, (width, height))
+    input_data = np.expand_dims(image_resized, axis=0)
+    print("starting1")
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    print("starting3")
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
+        0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
+    print("starting4")
+    print(len(scores))
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
 
-        # Draw label
-        object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
-        label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
-        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-        label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
-        cv2.rectangle(image, (xmin, label_ymin - labelSize[1] - 10),
-                      (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
-                      cv2.FILLED)  # Draw white box to put label text in
-        cv2.putText(image, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
-                    2)  # Draw label text
+            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
-# All the results have been drawn on the image, now display the image
-print("displaying image")
-cv2.imshow('Object detector', image)
-cv2.imwrite('/Users/anil/Desktop/frame-test-result.jpg', image)
+            # Draw label
+            object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+            cv2.rectangle(image, (xmin, label_ymin - labelSize[1] - 10),
+                          (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
+                          cv2.FILLED)  # Draw white box to put label text in
+            cv2.putText(image, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
+                        2)  # Draw label text
+
+    # All the results have been drawn on the image, now display the image
+    print("displaying image")
+    cv2.imwrite('/Users/anil/Desktop/captured/motion{:d}.jpg'.format(count), image)
+    count += 1
 
 # Clean up
 # cv2.destroyAllWindows()
