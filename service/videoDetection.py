645c078f944c288b6b643584c1ef992d8944ab02
anil <anil.kumar.ait09@gmail.com>
2022-05-29 23:33:49 +1000

    Squirrel-AI - skipped sharpening of image and added skipping frames

--- service/videoDetection.py
+++ service/videoDetection.py
@@ -83,13 +83,17 @@ def main_method(videoUrl):
         while ret:
             file_processed = 1
             sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-            sharpened_image = cv2.filter2D(image, -1, sharpen_kernel)
+            cv2.filter2D(image, -1, sharpen_kernel)
+            sharpened_image = image
             if tensor_coco_ssd_mobilenet(sharpened_image, ssd_model_path, logging) \
                     and perform_object_detection(sharpened_image, efficientdet_lite0_path, bool(0), logging):
                 process_face(sharpened_image, frame_count)
                 cv2.imwrite('/usr/local/squirrel-ai/visitor/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg',
                             sharpened_image)
+            frame_count += 5  # i.e. at 30 fps, this advances one second
+            capture.set(cv2.CAP_PROP_POS_FRAMES, frame_count)
             ret, image = capture.read()
+
     else:
         capture.release()
         logging.debug("released {0}".format(videoUrl))
