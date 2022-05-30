3e5bb2f130aef8db56a5ceec4e03cc4d394b7acc
anil <anil.kumar.ait09@gmail.com>
2022-05-30 17:49:01 +1000

    Squirrel-AI - code refactoring

--- service/videoDetection.py
+++ service/videoDetection.py
@@ -10,8 +10,21 @@ import os
 # Initializing things
 from detection.tensorflow.tf_coco_ssd_algorithm import tensor_coco_ssd_mobilenet
 from detection.tensorflow.tf_lite_algorithm import perform_object_detection
-from detection.opencv.detection_util import is_human_present
 import logging
+
+
+MOTION_VIDEO_URL = '/var/lib/motion/*'
+CONFIG_PROPERTIES = '/usr/local/squirrel-ai/config.properties'
+ARCHIVE_URL = "/usr/local/squirrel-ai/data/archives/"
+
+# For writing
+UNKNOWN_VISITORS_PATH = '/usr/local/squirrel-ai/result/unknown-visitors/'
+CAPTURED_CRIMINALS_PATH = '/usr/local/squirrel-ai/result/captured-criminals/'
+KNOWN_VISITORS_PATH = '/usr/local/squirrel-ai/result/known-visitors/'
+
+# For reading
+FAMILIAR_FACES_PATH = '/usr/local/squirrel-ai/data/familiar-faces/*'
+WANTED_CRIMINALS_PATH = '/usr/local/squirrel-ai/data/wanted-criminals/*'
 
 count = 0
 criminal_cache = []
@@ -28,7 +41,7 @@ ssd_model_path = '/usr/local/squirrel-ai/model/coco-ssd-mobilenet'
 efficientdet_lite0_path = '/usr/local/squirrel-ai/model/efficientdet-lite0/efficientdet_lite0.tflite'
 
 startDateTime = datetime.now()
-for eachWantedCriminalPath in glob.glob('/usr/local/squirrel-ai/wanted-criminals/*'):
+for eachWantedCriminalPath in glob.glob(WANTED_CRIMINALS_PATH):
     criminal_image = load_image_file(eachWantedCriminalPath)
     criminal_image_encoding = face_encodings(criminal_image)[0]
     criminal_cache.append(criminal_image_encoding)
@@ -37,7 +50,7 @@ endDateTime = datetime.now()
 logging.info("Loaded criminal  {0} images in {1} seconds".format(len(criminal_cache), (endDateTime - startDateTime)))
 
 startDateTime = datetime.now()
-for eachWantedKnownPersonPath in glob.glob('/usr/local/squirrel-ai/known/*'):
+for eachWantedKnownPersonPath in glob.glob(FAMILIAR_FACES_PATH):
     known_person_image = load_image_file(eachWantedKnownPersonPath)
     known_person_image_encoding = face_encodings(known_person_image)[0]
     known_person_cache.append(known_person_image_encoding)
@@ -56,17 +69,17 @@ def process_face(image, count_index):
         for each_criminal_encoding in criminal_cache:
             if compare_faces_with_encodings(each_criminal_encoding, unknown_face_image_encodings,
                                             "eachWantedCriminalPath"):
-                cv2.imwrite('/usr/local/squirrel-ai/captured/crimininal-frame{:d}.jpg'.format(count_index),
+                cv2.imwrite('{}criminal-frame{:d}.jpg'.format(CAPTURED_CRIMINALS_PATH, count_index),
                             unknown_face_image)
 
         for each_known_encoding in known_person_cache:
             if compare_faces_with_encodings(each_known_encoding, unknown_face_image_encodings,
                                             "eachWantedKnownPath"):
-                cv2.imwrite('/usr/local/squirrel-ai/captured/known-frame{:d}.jpg'.format(count_index),
+                cv2.imwrite('{}known-frame{:d}.jpg'.format(KNOWN_VISITORS_PATH, count_index),
                             unknown_face_image)
         end_date_time = datetime.now()
 
-        cv2.imwrite('/usr/local/squirrel-ai/captured/face-frame{:d}.jpg'.format(count_index),
+        cv2.imwrite('/usr/local/squirrel-ai/result/unknown-visitors/face-frame{:d}.jpg'.format(count_index),
                     unknown_face_image)
         logging.debug("Total comparison time is {0} seconds".format((end_date_time - start_date_time)))
         count_index += 1
@@ -90,7 +103,7 @@ def main_method(videoUrl):
                     and perform_object_detection(image, efficientdet_lite0_path, bool(0), logging):
                 logging.debug("passed object detection".format(video_length))
                 process_face(image, frame_count)
-                cv2.imwrite('/usr/local/squirrel-ai/visitor/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg',
+                cv2.imwrite(UNKNOWN_VISITORS_PATH + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg',
                             image)
             ret, image = capture.read()
 
@@ -105,12 +118,12 @@ def main_method(videoUrl):
 def archive_file(each_video_url):
     logging.info("Archiving {0}".format(each_video_url))
     file_name = os.path.basename(each_video_url)
-    os.rename(each_video_url, "/usr/local/squirrel-ai/archives/" + file_name)
+    os.rename(each_video_url, ARCHIVE_URL + file_name)
 
 
 def set_config_level():
     config = configparser.ConfigParser()
-    config.read('/usr/local/squirrel-ai/config.properties')
+    config.read(CONFIG_PROPERTIES)
     log_level = config.read('log.level')
     if log_level == 'DEBUG':
         logging.getLogger().setLevel(logging.DEBUG)
@@ -121,7 +134,7 @@ def set_config_level():
 set_config_level()
 try:
     while True:
-        for eachVideoUrl in glob.glob('/var/lib/motion/*'):
+        for eachVideoUrl in glob.glob(MOTION_VIDEO_URL):
             logging.info("Processing {0}".format(eachVideoUrl))
             main_method(eachVideoUrl)
 except Exception as e:
