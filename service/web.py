5a3edcb264b19c46842843f439c718daa51cca66
anil <anil.kumar.ait09@gmail.com>
2022-07-11 23:29:56 +1000

    Squirrel-AI -  Added a controller

--- service/web.py
+++ service/web.py
@@ -1,6 +1,8 @@
+import logging
+
 from flask import Flask, request, jsonify
 
-from videoDetection import load_criminal_images, load_known_images, set_config_level
+from videoDetection import load_criminal_images, load_known_images, set_config_level, MOTION_VIDEO_URL, main_method
 
 load_criminal_images()
 load_known_images()
@@ -8,6 +10,10 @@ set_config_level()
 app = Flask(__name__)
 
 
-@app.get("/countries")
+@app.get("/trigger-analysis")
 def get_countries():
-    return jsonify("hello")
+    video_file_name = request.args.get('file')
+    logging.info("Video file name {0}".format(video_file_name))
+    complete_video_path = MOTION_VIDEO_URL+video_file_name
+    main_method(complete_video_path)
+    return jsonify("success")
