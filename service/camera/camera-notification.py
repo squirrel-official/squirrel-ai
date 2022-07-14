c4eac67859764287bedb03518f5a6bf0807cdb5f
anil <anil.kumar.ait09@gmail.com>
2022-07-15 00:02:44 +1000

    Squirrel-AI - change config files to directly send the notifications

--- service/camera/camera-notification.py
+++ service/camera/camera-notification.py
@@ -6,10 +6,10 @@ logging.basicConfig(filename='/usr/local/squirrel-ai/logs/service.log',
                     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %('
                            'funcName)s: %(message)s', level=logging.DEBUG,
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-logging.info("Inside the Analyze video notification having argument {}".format(len(sys.argv)))
+NOTIFICATION_URL = 'http://my-security.local:8087/notification?camera-id=GATE-CAMERA'
 
 if len(sys.argv) == 2:
     logging.info(sys.argv[1])
-    NOTIFICATION_URL = 'http://127.0.0.1:5000/trigger-analysis?file='+sys.argv[1]
-    data = requests.post(NOTIFICATION_URL, "Trigger analysis")
+    NOTIFICATION_URL = 'http://my-security.local:8087/notification?camera-id='+sys.argv[1]
+
+data = requests.post(NOTIFICATION_URL, "General Camera Information")
