464eaee1bb8fa4f2a6e383dbc6afd5ab00ad4458
anil <anil.kumar.ait09@gmail.com>
2022-07-19 23:06:50 +1000

    Squirrel-AI - changed logger code

--- service/web.py
+++ service/web.py
@@ -4,25 +4,29 @@ from flask import Flask, request, jsonify
 from waitress import serve
 import configparser
 
-from videoDetection import load_criminal_images, load_known_images, set_config_level, main_method, CONFIG_PROPERTIES
+from videoDetection import load_criminal_images, load_known_images, main_method, CONFIG_PROPERTIES
 
-logging.basicConfig(filename='/usr/local/squirrel-ai/logs/service.log',
-                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %('
-                           'funcName)s: %(message)s', level=logging.DEBUG,
-                    datefmt='%Y-%m-%d %H:%M:%S')
-config = configparser.ConfigParser()
-config.read(CONFIG_PROPERTIES)
-log_level = config['DEFAULT']['log.level']
-logging.info("changing log level to {0}".format(log_level))
-logger = logging.getLogger("WebController");
-if log_level == 'DEBUG':
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.ERROR)
-
-load_criminal_images()
-load_known_images()
 app = Flask(__name__)
+
+
+def load_logging_level():
+    logging.basicConfig(filename='/usr/local/squirrel-ai/logs/service.log',
+                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %('
+                               'funcName)s: %(message)s', level=logging.DEBUG,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PROPERTIES)
+    log_level = config['DEFAULT']['log.level']
+    logging.info("changing log level to {0}".format(log_level))
+    weblogger = logging.getLogger("WebController")
+    if log_level == 'DEBUG':
+        weblogger.setLevel(logging.DEBUG)
+    else:
+        weblogger.setLevel(logging.ERROR)
+    return weblogger
+
+
+logger = load_logging_level()
 
 
 @app.route("/")
@@ -39,4 +43,6 @@ def analyze_video():
 
 
 if __name__ == '__main__':
+    load_criminal_images()
+    load_known_images()
     serve(app, host="0.0.0.0", port=5000)
