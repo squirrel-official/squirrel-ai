bd41a2e563bfb5fc7da0529f389897206d261076
anil <anil.kumar.ait09@gmail.com>
2022-07-25 18:11:57 +1000

    Squirrel-AI - Code refactoring(in progress)

--- service/motionDetection.py
+++ service/motionDetection.py
@@ -2,7 +2,6 @@
 import cv2
 from datetime import datetime
 from customLogging.customLogging import get_logger
-import os
 # Initializing things
 from detection.tensorflow.tf_coco_ssd_algorithm import tensor_coco_ssd_mobilenet
 from detection.tensorflow.tf_lite_algorithm import perform_object_detection
@@ -18,6 +17,7 @@ MOTION_VIDEO_URL = '/var/lib/motion/*'
 UNKNOWN_VISITORS_PATH = '/usr/local/squirrel-ai/result/unknown-visitors/'
 
 GARAGE_EXTERNAL_CAMERA_STREAM = 'http://my-security.local:7776/1/stream'
+GATE_EXTERNAL_CAMERA_STREAM = 'http://my-security.local:7776/2/stream'
 NOTIFICATION_URL = 'http://my-security.local:8087/notification?camera-id='
 count = 0
 criminal_cache = []
@@ -54,6 +54,7 @@ def start_monitoring():
         load_criminal_images()
         load_known_images()
         analyze_each_video(GARAGE_EXTERNAL_CAMERA_STREAM, 1)
+        analyze_each_video(GATE_EXTERNAL_CAMERA_STREAM, 2)
 
     except Exception as e:
         logger.error("An exception occurred.")
