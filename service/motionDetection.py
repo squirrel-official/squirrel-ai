319a7468000d0a271ac617bb16f9e28d2ef85798
anil <anil.kumar.ait09@gmail.com>
2022-09-03 23:22:31 +1000

    Squirrel-AI - Removed unnecessary code

--- service/motionDetection.py
+++ service/motionDetection.py
@@ -24,14 +24,13 @@ def monitor_camera_stream(streamUrl, criminal_cache, known_person_cache):
             logger.error("Error opening video file {}".format(streamUrl))
 
         frame_count = 1
-        object_detection_flag = 0
         if capture.isOpened():
             ret, image = capture.read()
             logger.info(" Processing file {0} ".format(streamUrl))
             while ret:
                 if tensor_coco_ssd_mobilenet(image, ssd_model_path) \
                         and perform_object_detection(image, efficientdet_lite0_path, bool(0)):
-                    logger.debug("Object detected, flag :{0}".format(object_detection_flag))
+                    logger.debug("Object detected")
                     analyze_face(image, frame_count, criminal_cache, known_person_cache)
                 ret, image = capture.read()
     except Exception as e:
