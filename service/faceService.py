d55e278f30e2ccd677da257646a82eebd0bf3db6
anil <anil.kumar.ait09@gmail.com>
2022-09-20 22:40:08 +1000

    Squirrel-AI - Profile changes

--- service/faceService.py
+++ service/faceService.py
@@ -41,7 +41,11 @@ def analyze_face(image, count_index, criminal_cache, known_person_cache):
                             unknown_face_image)
                 cv2.imwrite('{}criminal-frame{:d}.jpg'.format(CAPTURED_CRIMINALS_PATH, count_index),
                             image)
-                requests.post(CRIMINAL_NOTIFICATION_URL)
+                try:
+                    requests.post(CRIMINAL_NOTIFICATION_URL)
+                except Exception as e:
+                    logger.error("An error happened {0}", e)
+                    pass
 
         for each_known_encoding in known_person_cache:
             if compare_faces_with_encodings(each_known_encoding, unknown_face_image_encodings,
@@ -51,7 +55,12 @@ def analyze_face(image, count_index, criminal_cache, known_person_cache):
                             unknown_face_image)
                 cv2.imwrite('{}known-frame{:d}.jpg'.format(KNOWN_VISITORS_PATH, count_index),
                             image)
-                requests.post(FRIEND_NOTIFICATION_URL)
+                try:
+                    requests.post(FRIEND_NOTIFICATION_URL)
+                except Exception as e:
+                    logger.error("An error happened {0}", e)
+                    pass
+
         logger.debug("Total comparison time is {0} seconds".format((time.time() - start_date_time)))
         count_index += 1
 
