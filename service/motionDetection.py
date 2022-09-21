91d172701cb492cb1f4bbec2c556cf7e8e90b248
anil <anil.kumar.ait09@gmail.com>
2022-09-21 21:29:02 +1000

    Squirrel-AI - Exposure settings and fps

--- service/motionDetection.py
+++ service/motionDetection.py
@@ -34,9 +34,10 @@ def monitor_camera_stream(streamUrl, criminal_cache, known_person_cache):
         if capture.isOpened():
             ret, image = capture.read()
             logger.info(" Processing file {0} ".format(streamUrl))
+            logger.info("Frame rate: {0} ".format(capture.get(cv2.CAP_PROP_FPS)))
             while ret:
                 cv2.imwrite('{}All-frame{:d}.jpg'.format('/usr/local/squirrel-ai/result/captured-criminals/', random.randint(0, 1000)), image)
-                logger.info("Frame rate: {0} ".format(capture.get(cv2.CAP_PROP_FPS)))
+
                 if tensor_coco_ssd_mobilenet(image, ssd_model_path) \
                         and perform_object_detection(image, efficientdet_lite0_path, bool(0)):
                     logger.debug("Object detected")
