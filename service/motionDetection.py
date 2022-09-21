586cfc435e8cea909bb93ffe054cd67792720704
anil <anil.kumar.ait09@gmail.com>
2022-09-21 21:36:50 +1000

    Squirrel-AI - removed tensor_coco_ssd_mobilenet for faster detection

--- service/motionDetection.py
+++ service/motionDetection.py
@@ -38,8 +38,7 @@ def monitor_camera_stream(streamUrl, criminal_cache, known_person_cache):
             while ret:
                 cv2.imwrite('{}All-frame{:d}.jpg'.format('/usr/local/squirrel-ai/result/captured-criminals/', random.randint(0, 1000)), image)
 
-                if tensor_coco_ssd_mobilenet(image, ssd_model_path) \
-                        and perform_object_detection(image, efficientdet_lite0_path, bool(0)):
+                if perform_object_detection(image, efficientdet_lite0_path, bool(0)):
                     logger.debug("Object detected")
                     analyze_face(image, frame_count, criminal_cache, known_person_cache)
                 ret, image = capture.read()
