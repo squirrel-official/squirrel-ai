5599a897b4934c64a0ae76c58333383977f43adc
anil <anil.kumar.ait09@gmail.com>
2023-02-26 20:30:32 +1100

    Squirrel-AI -Just motion setup

--- setup/documentation/motion-setup.sh
+++ setup/documentation/motion-setup.sh
@@ -2,21 +2,16 @@ sudo chmod -R 777 /var/log/
 #Install Motion
 sudo apt-get install motion -y
 
-#Add permissions and append the line to bottom of file
-sudo visudo
-
-root  ALL=(ALL:ALL) NOPASSWD: /usr/local/person-service/src/main/resources/detection.sh
-root  ALL=(ALL:ALL) NOPASSWD: /usr/local/person-service/src/main/resources/motion-restart.sh
-pi  ALL=(ALL:ALL) NOPASSWD: /usr/sbin/service  motion stop
-pi  ALL=(ALL:ALL) NOPASSWD: /usr/sbin/service  motion start
-
-
 #Update motion permissions
-chmod -R 777 /usr/local/person-service/src/main/resources/detection.sh
-chmod -R 777 /usr/local/person-service/src/main/resources/motion-restart.sh
 chmod -R 777 /var/log/motion
 chmod -R 777 /var/lib/motion
 sudo chmod -R 777 /etc/motion
+
+#enable Motion on reboot
+sudo systemctl enable motion
+
+
+
 
 
 
